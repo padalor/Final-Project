a9739434b8e86cdaf90e0536ce50406d6dbba60f
padalor <55326313+padalor@users.noreply.github.com>
2020-05-15 18:53:43 -0500

    Add files via upload

---
+++ finalProject.py
@@ -0,0 +1,27 @@
+'''
+Created on May 15, 2020
+
+@author: ITAUser
+'''
+
+#PSEUDOCODE
+
+#display the game instructions
+
+#create players
+
+#create empty tic tac toe board
+
+#display board
+
+#user picks spot on the board
+
+#computer picks spot on the board
+
+#repeat 
+
+#if player gets 3 in a row they win
+
+#if computer gets 3 in a row computer wins
+
+#if no one wins then its a tie
